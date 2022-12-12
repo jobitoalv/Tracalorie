1bf1e89c757ede7aa28d02b5d5ac8730ec02cd60
Job Alvarez <jobitoalv@gmail.com>
2022-12-12 11:42:01 +1000

    add return to public methods

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
 // Storage Controller
 
-//Item Controller
+
+// Item Controller
 const ItemCtrl = (function(){
+    // Item Constructor
     const Item = function(id, name, calories){
-        this.id = id;
-        this.name = name;
-        this.calories = calories;
+      this.id = id;
+      this.name = name;
+      this.calories = calories;
     }
-
-// Data Structure / State
+  
+    // Data Structure / State
     const data = {
-        item: [
-            {id: 0,
-             name: 'Steak Dinner',
-             calories: 1200},
-            {id: 1,
-             name: 'Cookies',
-             calories: 400},
-            {id: 2,
-             name: 'Eggs',
-             calories: 300}
-        ],
-        currentItem: null,
-        totalCalories: 0
+      items: [
+        {id: 0, name: 'Steak Dinner', calories: 1200},
+        {id: 1, name: 'Cookie', calories: 400},
+        {id: 2, name: 'Eggs', calories: 300}
+      ],
+      currentItem: null,
+      totalCalories: 0
     }
-})();
-
-//UI Controller
-
-const UICtrl = (function(){
-
-})();
-
-//App Controller 
-
-const AppCtrl = (function(itemCtrl,UICtrl){
-
-})(itemCtrl,UICtrl);
+  
+    // Public methods
+    return {
+      logData: function(){
+        return data;
+      }
+    }
+  })();
+  
+  
+  
+  // UI Controller
+  const UICtrl = (function(){
+    
+    // Public methods
+    return {
+  
+    }
+  })();
+  
+  
+  
+  // App Controller
+  const App = (function(ItemCtrl, UICtrl){
+  
+    // Public methods
+    return {
+      init: function(){
+        console.log('Initializing App...');
+      }
+    }
+    
+  })(ItemCtrl, UICtrl);
+  
+  // Initialize App
+  App.init();
